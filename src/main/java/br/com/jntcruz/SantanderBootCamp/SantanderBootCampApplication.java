17e882b132bbb79faecf0edfb43958d24f65c85a
Jonatas <jonatascruz.99@gmail.com>
2023-09-03 20:08:36 -0300

    Initial commit

---
+++ src/main/java/br/com/jntcruz/SantanderBootCamp/SantanderBootCampApplication.java
@@ -0,0 +1,13 @@
+package br.com.jntcruz.SantanderBootCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jntcruz/SantanderBootCamp/SantanderBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jntcruz.SantanderBootCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
